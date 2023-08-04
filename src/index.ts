40c47040537bc135b5f380d14355fe25fa03741d
afeiship <1290657123@qq.com>
2023-08-04 10:41:42 +0800

    feat: init deps

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import httpRestConfig from '@jswork/http-rest-config';
-
-export default (inConfig, inRequest): any => {
-  const context = {};
-  httpRestConfig(context, inRequest, inConfig);
-  return context;
-};
